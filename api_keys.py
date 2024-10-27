feb25772da0b905c2907f6f6e33426d80e969833
Maha112024 <mahapentakota@gmail.com>
2024-10-27 17:50:27 -0500

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "c889e9012433734597c3bca8e3944fe5"
+weather_api_key = "Your key"
 
 # Geoapify API Key
-geoapify_key = "18de866a346643738d1e1900a4b902c6"
+geoapify_key = "your key"
